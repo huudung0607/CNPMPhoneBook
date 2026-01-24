923464da9fd606fd45b1ed521f520e02342790b6
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2026-01-24 17:44:09 +0700

    add group and phonebook

--- source.cpp
+++ source.cpp
@@ -333,6 +333,536 @@ public:
 	}
 };
 
+//////////Quan li Group//////////
+struct Group {
+	string id;
+	string name;
+	vector<Contact> member;
+	friend ostream& operator << (ostream& out, Group x) {
+		out << "Group ID : " << x.id << endl;
+		out << "Group Name : " << x.name << endl;
+		out << "Group Member : " << x.member.size();
+		return out;
+	}
+};
+vector<Group> groupList;
+set<string> groupIDCheck;
+set<string> groupNameCheck;
+/////////////////////////////////
+
+
+class phoneBook {
+private:
+	string bookID;
+	vector<Contact> contactList;
+public:
+	phoneBook() {};
+	phoneBook(string bookID) {
+		this->bookID = bookID;
+	}
+	/////PhoneBook Features
+	void viewContactList() {
+		for (int i = 0; i < contactList.size(); i++) {
+			cout << "========================================" << endl;
+			contactList[i].displayContact();
+			cout << "========================================" << endl;
+			cout << endl;
+		}
+	}
+	void getContactInformation() {
+		cout << "Chon phuong thuc tra : ";
+		cout << "1. So dien thoai : ";
+		cout << "2. Email : ";
+		string ch;
+		getline(cin, ch);
+		while (ch != "1" && ch != "2") {
+			cout << "Khong hop le, vui long nhap lai : ";
+			getline(cin, ch);
+		}
+		if (ch == "1") {
+			cout << "Nhap so dien thoai nguoi lien he : ";
+			string soDienThoai;
+			getline(cin, soDienThoai);
+			while (!checkPhoneNumber(soDienThoai) || !phoneNumberCheck.count(soDienThoai)) {
+				cout << "So dien thoai khong hop le, vui long nhap lai : ";
+				getline(cin, soDienThoai);
+			}
+			for (int i = 0; i < contactList.size(); i++) {
+				if (contactList[i].getPhoneNumber() == soDienThoai) {
+					cout << "========================================" << endl;
+					contactList[i].displayContact();
+					cout << "========================================" << endl;
+					cout << endl;
+				}
+			}
+		}
+		else if (ch == "2") {
+			cout << "Nhap email nguoi lien he : ";
+			string emailNLH;
+			getline(cin, emailNLH);
+			while (!checkEmail(emailNLH) || !emailCheck.count(emailNLH)) {
+				cout << "Email khong hop le, vui long nhap lai : ";
+				getline(cin, emailNLH);
+			}
+			for (int i = 0; i < contactList.size(); i++) {
+				if (contactList[i].getEmail() == emailNLH) {
+					cout << "========================================" << endl;
+					contactList[i].displayContact();
+					cout << "========================================" << endl;
+					cout << endl;
+				}
+			}
+		}
+	}
+	static bool cmp(Contact a, Contact b) {
+		if (a.getContactFirstLetter() == b.getContactFirstLetter()) {
+			return a.getFullName().length() < b.getFullName().length();
+		}
+		return a.getContactFirstLetter() < b.getContactFirstLetter();
+	}
+	static bool checkName(string fullName, string tenNhap) {
+		vector<string> fullname;
+		vector<string> tennhap;
+		stringstream ss1(fullName);
+		stringstream ss2(tenNhap);
+		string tmp1, tmp2;
+		while (ss1 >> tmp1) {
+			fullname.push_back(tmp1);
+		}
+		while (ss2 >> tmp2) {
+			tennhap.push_back(tmp2);
+		}
+		for (string x : tennhap) {
+			if (find(fullname.begin(), fullname.end(), x) == fullname.end()) {
+				return false;
+			}
+		}
+		return true;
+	}
+	void sortContactByName() {
+		sort(contactList.begin(), contactList.end(), cmp);
+		viewContactList();
+	}
+	void addContact() {
+		Contact x;
+		cin >> x;
+		contactList.push_back(x);
+	}
+	void editContact() {
+		cout << "Nhap ten contact ban muon chinh sua : ";
+		string ten;
+		getline(cin, ten);
+		while (!checkUsername(ten)) {
+			cout << "Ten khong hop le, vui long nhap lai :";
+			getline(cin, ten);
+		}
+		chuanHoaTen(ten);
+		cout << "Danh sach nguoi co ten " << ten << endl;
+		for (int i = 0; i < contactList.size(); i++) {
+			if (checkName(contactList[i].getFullName(), ten)) {
+				contactList[i].displayContact();
+			}
+		}
+		cout << "Nhap id nguoi ban muon chinh sua : ";
+		string id;
+		getline(cin, id);
+		while (!checkContactID(id) && userIDCheck.find(id) == userIDCheck.end()) {
+			cout << "ID khong hop le, vui long nhap lai :";
+			getline(cin, id);
+		}
+		for (int i = 0; i < contactList.size(); i++) {
+			if (contactList[i].getContactID() == id) {
+				contactList[i].displayContact();
+				while (true) {
+					cout << "Chon thong tin ban muon chinh sua : " << endl;
+					cout << "1. Full Name" << endl;
+					cout << "2. So Dien Thoai" << endl;
+					cout << "3. Email" << endl;
+					cout << "4. Address" << endl;
+					cout << "5. Company" << endl;
+					cout << "6. Relative" << endl;
+					cout << "0. Thoat chinh chua" << endl;
+					string ch;
+					getline(cin, ch);
+					while (ch != "1" && ch != "2" && ch != "3" && ch != "4" && ch != "5" && ch != "6" && ch != "0") {
+						cout << "Lua chon khong hop le, vui long nhap lai : ";
+						getline(cin, ch);
+					}
+					if (ch == "1") {
+						cout << "Nhap ten thay doi : ";
+						string ten;
+						getline(cin, ten);
+						while (!checkUsername(ten)) {
+							cout << "Ten khong hop le, vui long nhap lai : ";
+							getline(cin, ten);
+						}
+						contactList[i].setFullName(ten);
+						cout << endl;
+					}
+					else if (ch == "2") {
+						cout << "Nhap so dien thoai thay doi : ";
+						string sdt;
+						getline(cin, sdt);
+						while (!checkPhoneNumber(sdt)) {
+							cout << "So dien thoai khong hop le, vui long nhap lai : ";
+							getline(cin, sdt);
+						}
+						contactList[i].setPhoneNumber(sdt);
+						cout << endl;
+					}
+					else if (ch == "3") {
+						cout << "Nhap email thay doi : ";
+						string email;
+						getline(cin, email);
+						while (!checkEmail(email)) {
+							cout << "Email khong hop le, vui long nhap lai : ";
+							getline(cin, email);
+						}
+						contactList[i].setEmail(email);
+						cout << endl;
+					}
+					else if (ch == "4") {
+						cout << "Nhap dia chi thay doi : ";
+						string address;
+						getline(cin, address);
+						contactList[i].setAddress(address);
+						cout << endl;
+					}
+					else if (ch == "5") {
+						cout << "Nhap cong ty thay doi : ";
+						string company;
+						getline(cin, company);
+						contactList[i].setCompany(company);
+						cout << endl;
+					}
+					else if (ch == "6") {
+						cout << "Nhap quan he thay doi : ";
+						string relatives;
+						getline(cin, relatives);
+						contactList[i].setRelatives(relatives);
+						cout << endl;
+					}
+					else if (ch == "0") {
+						cout << "Thoat chinh sua!" << endl;
+						cout << endl;
+						break;
+					}
+				}
+				cout << "Thong tin sau khi chinh sua : " << endl;
+				contactList[i].displayContact();
+			}
+		}
+	}
+	void deleteContact() {
+		cout << "Nhap so dien thoai nguoi lien he ban muon xoa : ";
+		string sdt;
+		getline(cin, sdt);
+		while (!checkPhoneNumber(sdt) || !phoneNumberCheck.count(sdt)) {
+			cout << "So dien thoai khong hop le, vui long nhap lai : ";
+			getline(cin, sdt);
+		}
+		for (int i = 0; i < contactList.size(); i++) {
+			if (contactList[i].getPhoneNumber() == sdt) {
+				contactList.erase(contactList.begin() + i);
+				cout << "Xoa tai khoan thanh cong ! ";
+			}
+		}
+		cout << endl;
+		cout << "Danh sach nguoi lien he sau khi chinh sua : " << endl;
+		viewContactList();
+	}
+	void addRelationship() {
+		cout << "Nhap so dien thoai nguoi lien he ban muon them contact : ";
+		string sdt;
+		getline(cin, sdt);
+		while (!checkPhoneNumber(sdt) || !phoneNumberCheck.count(sdt)) {
+			cout << "So dien thoai khong hop le, vui long nhap lai : ";
+			getline(cin, sdt);
+		}
+		cout << "Nhap moi lien he : ";
+		string rela;
+		getline(cin, rela);
+		while (!checkUsername(rela)) {
+			cout << "Moi lien he khong hop le, vui long nhap lai : ";
+			getline(cin, rela);
+		}
+		for (int i = 0; i < contactList.size(); i++) {
+			if (contactList[i].getPhoneNumber() == sdt) {
+				contactList[i].setRelatives(rela);
+			}
+		}
+		cout << "Danh sach nguoi lien he sau khi chinh sua : ";
+		viewContactList();
+	}
+	void searchContact() {
+		cout << "1. Tim theo ten : ";
+		cout << "2. Tim theo sdt : ";
+		string choice;
+		getline(cin, choice);
+		while (choice != "1" && choice != "2") {
+			cout << "Khong hop le, vui long nhap lai ";
+			getline(cin, choice);
+		}
+		if (choice == "1") {
+			cout << "Nhap ten contact : ";
+			string ten;
+			getline(cin, ten);
+			while (!checkUsername(ten)) {
+				cout << "Ten khong hop le, vui long nhap lai :";
+				getline(cin, ten);
+			}
+			chuanHoaTen(ten);
+			cout << "Danh sach nguoi co ten " << ten << endl;
+			for (int i = 0; i < contactList.size(); i++) {
+				if (checkName(contactList[i].getFullName(), ten)) {
+					contactList[i].displayContact();
+				}
+			}
+		}
+		else {
+			cout << "Nhap so dien thoai : ";
+			string sdt;
+			getline(cin, sdt);
+			while (!checkPhoneNumber(sdt) || !phoneNumberCheck.count(sdt)) {
+				cout << "So dien thoai khong hop le, vui long nhap lai : ";
+				getline(cin, sdt);
+			}
+			for (int i = 0; i < contactList.size(); i++) {
+				if (contactList[i].getPhoneNumber() == sdt) {
+					contactList[i].displayContact();
+				}
+			}
+		}
+	}
+	void viewContact() {
+		cout << "1. Tim theo ten : ";
+		cout << "2. Tim theo sdt : ";
+		string choice;
+		getline(cin, choice);
+		while (choice != "1" && choice != "2") {
+			cout << "Khong hop le, vui long nhap lai ";
+			getline(cin, choice);
+		}
+		if (choice == "1") {
+			cout << "Nhap ten contact : ";
+			string ten;
+			getline(cin, ten);
+			while (!checkUsername(ten)) {
+				cout << "Ten khong hop le, vui long nhap lai :";
+				getline(cin, ten);
+			}
+			chuanHoaTen(ten);
+			cout << "Danh sach nguoi co ten " << ten << endl;
+			for (int i = 0; i < contactList.size(); i++) {
+				if (checkName(contactList[i].getFullName(), ten)) {
+					contactList[i].displayContact();
+				}
+			}
+		}
+		else {
+			cout << "Nhap so dien thoai : ";
+			string sdt;
+			getline(cin, sdt);
+			while (!checkPhoneNumber(sdt) || !phoneNumberCheck.count(sdt)) {
+				cout << "So dien thoai khong hop le, vui long nhap lai : ";
+				getline(cin, sdt);
+			}
+			for (int i = 0; i < contactList.size(); i++) {
+				if (contactList[i].getPhoneNumber() == sdt) {
+					contactList[i].displayContact();
+				}
+			}
+		}
+	}
+	////Group Features
+	void showGroupList() {
+		for (int i = 0; i < groupList.size(); i++) {
+			cout << "===============================" << endl;
+			cout << groupList[i] << endl;
+			cout << "===============================" << endl;
+		}
+	}
+
+	//tao nhom
+	void makeGroup() {
+		srand(time(NULL));
+		Group group;
+		int id = rand() % 900 + 100;
+		while (groupIDCheck.count(to_string(id))) {
+			id = rand() % 900 + 100;
+		}
+		group.id = "G" + to_string(id);
+		groupIDCheck.insert(group.id);
+		string name;
+		getline(cin, name);
+		while (groupNameCheck.count(name)) {
+			cout << "Trung ten group, vui long nhap lai : ";
+			getline(cin, name);
+		}
+		group.name = name;
+		groupList.push_back(group);
+		cout << "Group da tao !" << endl;
+		cout << "Group ID : " << group.id << endl;
+		cout << "Group Name : " << group.name << endl;
+		cout << "Group Member : " << group.member.size() << endl;
+	}
+
+	//xoa nhom
+	void deleteGroup() {
+		cout << "Nhap ten nhom ban muon xoa : ";
+		string name;
+		getline(cin, name);
+		while (!groupNameCheck.count(name)) {
+			cout << "Khong ton tai ten nhom !, vui long nhap lai : ";
+			getline(cin, name);
+		}
+		for (int i = 0; i < groupList.size(); i++) {
+			if (groupList[i].name == name) {
+				groupList.erase(groupList.begin() + i);
+				cout << "Xoa nhom thanh cong ! " << endl;
+			}
+		}
+		showGroupList();
+	}
+
+	//them thanh vien vao nhom
+	void addMemberToGroup() {
+		// Kiểm tra tên nhóm và số điện thoại có tồn tại hay không
+		cout << "Nhap ten nhom : ";
+		string name; getline(cin, name);
+		while (!groupNameCheck.count(name)) {
+			cout << "Khong ton tai ten nhom, vui long nhap lai : ";
+			getline(cin, name);
+		}
+		cout << "Nhap so dien thoai Contact ban muon them vao nhom : ";
+		string sdt; getline(cin, sdt);
+		while (!phoneNumberCheck.count(sdt)) {
+			cout << "So dien thoai khong ton tai, vui long nhap lai : ";
+			getline(cin, sdt);
+		}
+
+
+		////////thêm 
+		for (int i = 0; i < groupList.size(); i++) {
+			if (groupList[i].name == name) {
+				for (int j = 0; j < contactList.size(); j++) {
+					if (contactList[j].getPhoneNumber() == sdt) {
+						groupList[i].member.push_back(contactList[j]);
+						break;
+					}
+				}
+			}
+			break;
+		}
+
+
+		///// in ra
+		cout << "Thanh vien nhom sau khi add : " << endl;
+		for (int i = 0; i < groupList.size(); i++) {
+			if (groupList[i].name == name) {
+				cout << groupList[i] << endl;
+				for (int j = 0; j < groupList[i].member.size(); j++) {
+					cout << "=================================" << endl;
+					groupList[i].member[j].displayContact();
+					cout << "=================================" << endl;
+				}
+			}
+		}
+	}
+
+	//xoa thanh vien khoi nhom
+	void deleteMemberFromGroup() {
+		cout << "Nhap ten nhom : ";
+		string name; getline(cin, name);
+		while (!groupNameCheck.count(name)) {
+			cout << "Khong ton tai ten nhom, vui long nhap lai : ";
+			getline(cin, name);
+		}
+		cout << "Nhap so dien thoai Contact ban muon xoa khoi nhom : ";
+		string sdt; getline(cin, sdt);
+		while (!phoneNumberCheck.count(sdt)) {
+			cout << "So dien thoai khong ton tai, vui long nhap lai : ";
+			getline(cin, sdt);
+		}
+		bool found = false;
+		for (int i = 0; i < groupList.size(); i++) {
+			if (groupList[i].name == name) {
+				for (int j = 0; j < groupList[i].member.size(); j++) {
+					if (groupList[i].member[j].getPhoneNumber() == sdt) {
+						groupList[i].member.erase(groupList[i].member.begin() + j);
+						found = true;
+						cout << endl;
+						break;
+					}
+				}
+			}
+			break;
+		}
+		if (!found) cout << "Nhom khong ton tai member co so dien thoai nay ! " << endl;
+		cout << endl;
+		cout << "Nhom sau khi xoa : " << endl;
+		for (int i = 0; i < groupList.size(); i++) {
+			if (groupList[i].name == name) {
+				for (int j = 0; j < groupList[i].member.size(); j++) {
+					cout << "=================================" << endl;
+					groupList[i].member[j].displayContact();
+					cout << "=================================" << endl;
+				}
+			}
+		}
+	}
+
+	//tim nhom
+	void searchGroup() {
+		cout << "Nhap ten nhom : ";
+		string name; getline(cin, name);
+		while (!groupNameCheck.count(name)) {
+			cout << "Khong ton tai ten nhom, vui long nhap lai : ";
+			getline(cin, name);
+		}
+		cout << endl;
+		for (int i = 0; i < groupList.size(); i++) {
+			if (groupList[i].name == name) {
+				cout << groupList[i] << endl;
+			}
+		}
+	}
+
+	// tim thanh vien trong nhom
+	void searchMemberInGroup() {
+		cout << "Nhap ten nhom : ";
+		string name; getline(cin, name);
+		while (!groupNameCheck.count(name)) {
+			cout << "Khong ton tai ten nhom, vui long nhap lai : ";
+			getline(cin, name);
+		}
+		cout << endl;
+		cout << "Nhap so dien thoai : ";
+		string sdt; getline(cin, sdt);
+		while (!checkPhoneNumber(sdt) || !phoneNumberCheck.count(sdt)) {
+			cout << "So dien thoai khong ton tai : ";
+			getline(cin, sdt);
+		}
+		bool found = false;
+		for (int i = 0; i < groupList.size(); i++) {
+			for (int j = 0; j < groupList[i].member.size(); j++) {
+				if (groupList[i].member[j].getPhoneNumber() == sdt) {
+					groupList[i].member[j].displayContact();
+				}
+			}
+		}
+		if (!found) cout << "Khong tim ra member trong nhom" << endl;
+	}
+};
+
+
+
+int main()
+{
+	
+}
+
+
 
 bool checkUsername(string username) {
 	if (isdigit(username[0])) return false;
@@ -386,10 +916,3 @@ bool checkContactID(string contactID) {
 	}
 	return true;
 }
-
-int main()
-{
-	
-}
-
-
